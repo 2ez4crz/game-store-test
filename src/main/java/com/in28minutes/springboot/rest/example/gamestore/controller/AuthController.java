0bed12670572eef825e68f7566cd67577fb1a3a2
Kristian Aditya Nugraha <kristian.nugraha@anabatic.com>
2018-10-19 15:27:38 +0700

    test

--- src/main/java/com/in28minutes/springboot/rest/example/gamestore/controller/AuthController.java
+++ src/main/java/com/in28minutes/springboot/rest/example/gamestore/controller/AuthController.java
@@ -17,9 +17,11 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.in28minutes.springboot.rest.example.gamestore.contract.LoginRequest;
 import com.in28minutes.springboot.rest.example.gamestore.contract.SignupRequest;
 import com.in28minutes.springboot.rest.example.gamestore.contract.UserPrincipal;
+import com.in28minutes.springboot.rest.example.gamestore.entity.Role;
 import com.in28minutes.springboot.rest.example.gamestore.entity.User;
 import com.in28minutes.springboot.rest.example.gamestore.security.CurrentUser;
 import com.in28minutes.springboot.rest.example.gamestore.security.JwtAuthenticationResponse;
+import com.in28minutes.springboot.rest.example.gamestore.service.RoleService;
 import com.in28minutes.springboot.rest.example.gamestore.service.UserService;
 
 @RestController
@@ -28,6 +30,8 @@ public class AuthController {
 	
 	@Autowired
 	UserService userService;
+	@Autowired
+	RoleService roleService; 
 	
 	@PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
@@ -46,5 +50,11 @@ public class AuthController {
     public String encodePass(@PathVariable String password) {
 		return userService.testEncode(password);
     }
-	
+	@PostMapping("/role/add")
+	public ResponseEntity<Object> addRole(@RequestBody Role role){
+		Role savedRole = roleService.addRole(role);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/role/add")
+				.buildAndExpand(savedRole.getId()).toUri();
+		return ResponseEntity.created(location).body("add role success!");
+	}
 }
